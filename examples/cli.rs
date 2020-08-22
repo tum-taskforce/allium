d15ac53a73996b7ad3a531d13088f6de6dc24044
Dominik Stolz <d.stolz@tum.de>
2020-08-23 00:44:44 +0200

    Allow specifying peers in CLI

--- examples/cli.rs
+++ examples/cli.rs
@@ -2,7 +2,8 @@ use onion::{Onion, Peer, RsaPrivateKey, RsaPublicKey};
 use std::env;
 use tokio::io::{self, BufReader};
 use tokio::prelude::*;
-use tokio::stream::{self, StreamExt};
+use tokio::stream::StreamExt;
+use tokio::sync::mpsc;
 
 const DEFAULT_ADDR: &str = "127.0.0.1:4200";
 
@@ -16,8 +17,8 @@ async fn main() {
         .unwrap();
     let hostkey = RsaPrivateKey::from_pem_file("testkey.pem").unwrap();
     let public_key = hostkey.public_key();
-    let peer_provider = stream::empty();
-    let (onion, mut events) = Onion::new(onion_addr, hostkey, peer_provider).unwrap();
+    let (mut peer_tx, peer_rx) = mpsc::unbounded_channel();
+    let (onion, mut events) = Onion::new(onion_addr, hostkey, peer_rx).unwrap();
 
     let mut stdin = BufReader::new(io::stdin()).lines();
     loop {
@@ -26,14 +27,19 @@ async fn main() {
                 println!("Event: {:?}", evt);
             }
             Some(line) = stdin.next() => {
-                parse_command(line.unwrap(), &onion, &public_key).await;
+                parse_command(line.unwrap(), &onion, &public_key, &mut peer_tx).await;
             }
             else => break,
         }
     }
 }
 
-async fn parse_command(cmd: String, onion: &Onion, hostkey: &RsaPublicKey) {
+async fn parse_command(
+    cmd: String,
+    onion: &Onion,
+    hostkey: &RsaPublicKey,
+    peers: &mut mpsc::UnboundedSender<Peer>,
+) {
     let mut parts = cmd.split_whitespace();
     match parts.next() {
         Some("build") => {
@@ -51,6 +57,11 @@ async fn parse_command(cmd: String, onion: &Onion, hostkey: &RsaPublicKey) {
             let tunnel_id = parts.next().unwrap().parse().unwrap();
             let data = parts.next().unwrap().as_bytes();
             onion.send_data(tunnel_id, data);
+        }
+        Some("peer") => {
+            let peer_addr = parts.next().unwrap().parse().unwrap();
+            let peer = Peer::new(peer_addr, hostkey.clone());
+            let _ = peers.send(peer);
         }
         Some("help") => {
             println!("Available Commands:");
